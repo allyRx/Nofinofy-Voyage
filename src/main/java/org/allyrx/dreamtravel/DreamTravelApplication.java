21e35bc89ac590cfaf768c3d83a6c14a54ed90a1
allyRx <herindrainyall2003@gmail.com>
2025-04-25 23:54:08 +0300

    init project

---
+++ src/main/java/org/allyrx/dreamtravel/DreamTravelApplication.java
@@ -0,0 +1,13 @@
+package org.allyrx.dreamtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/allyrx/dreamtravel/DreamTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.allyrx.dreamtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
