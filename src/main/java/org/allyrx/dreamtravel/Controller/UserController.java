02adbcbb0c2d7e52d999f5ddceb7c5497c9d8f6e
allyRx <herindrainyall2003@gmail.com>
2025-04-26 00:50:55 +0300

    add : all logic crud

--- src/main/java/org/allyrx/dreamtravel/Controller/UserController.java
+++ src/main/java/org/allyrx/dreamtravel/Controller/UserController.java
@@ -1,39 +1,51 @@
 package org.allyrx.dreamtravel.Controller;
 
+import lombok.AllArgsConstructor;
 import org.allyrx.dreamtravel.Entity.User;
+import org.allyrx.dreamtravel.Service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
 
+    //Injection du service
+    private final UserService userService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @GetMapping(consumes = APPLICATION_JSON_VALUE)
-    public void adduser(@RequestBody User user){}
+    public void adduser(@RequestBody User user){
+        userService.adduser(user);
+    }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public List<User> getAllUsers(){
-        return List.of();
+        return userService.getAllUsers();
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "{id}", produces = APPLICATION_JSON_VALUE)
-    public User getUserById(@PathVariable Integer id){
-        return null;
+    public Optional<User> getUserById(@PathVariable Integer id){
+        return userService.getUserById(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(path = "{id}" , consumes = APPLICATION_JSON_VALUE)
-    public void updateUser(@RequestBody User userUpdate , @PathVariable Integer id){}
+    public void updateUser(@RequestBody User userUpdate , @PathVariable Integer id){
+        userService.updateUser(userUpdate, id);
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping(path = "{id}")
-    public void deleteUserById(@PathVariable Integer id){}
+    public void deleteUserById(@PathVariable Integer id){
+        userService.deleteUserById(id);
+    }
 }
