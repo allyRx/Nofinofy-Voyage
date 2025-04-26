28ac8801fc5fede9795b1653c681318912447453
allyRx <herindrainyall2003@gmail.com>
2025-04-26 12:09:08 +0300

    add : entity places

---
+++ src/main/java/org/allyrx/dreamtravel/Entity/Places.java
@@ -0,0 +1,25 @@
+package org.allyrx.dreamtravel.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.allyrx.dreamtravel.Enum.EnumRating;
+
+import static jakarta.persistence.CascadeType.MERGE;
+import static jakarta.persistence.CascadeType.PERSIST;
+
+@Entity
+@Table(name = "Places") @Data
+public class Places {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String Name;
+    private String country;
+    private String city;
+    private Integer note;
+    private boolean visited;
+    private EnumRating rating;
+    @ManyToOne(cascade = {PERSIST, MERGE})
+    @JoinColumn(name = "user_id")
+    private User user;
+}
